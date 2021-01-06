21749cdc7d0b9796efab97d569b53aec8ea48f32
Vicky-Lam <vickylinyunqing@gmail.com>
2021-01-06 21:16:32 +0800

    First commit

--- Basemap.py
+++ Basemap.py
@@ -15,14 +15,7 @@ def plotMap():
     ax.set_extent([-180 ,180, -90, 90], crs=ccrs.PlateCarree())
 
     ax.add_feature(cfeature.LAND, facecolor='0.3')
-#    ax.add_feature(cfeature.LAKES, alpha=0.9)  
-#    ax.add_feature(cfeature.BORDERS, zorder=10)
-    ax.add_feature(cfeature.COASTLINE, zorder=10)
-
-#    states_provinces = cfeature.NaturalEarthFeature(
-#            category='cultural',  name='admin_1_states_provinces_lines',
-#            scale='50m', facecolor='none')
-#    ax.add_feature(states_provinces, edgecolor='black', zorder=10)   
+    ax.add_feature(cfeature.COASTLINE, zorder=10)  
 
     gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=2, color='black', alpha=0.5, linestyle='--', draw_labels=True)
     gl.top_labels =True 
